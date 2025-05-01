4e24a93537e2f876cf96cc43a3cdce763443be9d
ekwunifeifenna <ifennaekwunife73@gmail.com>
2025-05-01 08:08:35 -0400

    sql database bug fix

--- config.js
+++ config.js
@@ -5,12 +5,12 @@ module.exports = {
   HUGGINGFACE_API_KEY: process.env.HUGGINGFACE_API_KEY,
   AI_SERVICE_KEY: process.env.API_KEY || 'fs913x_uykFcLWSMeAjNTxS6DgikJfUW7LCt2whNHGQ', 
   DB_CONFIG: {
-    DB_HOST: process.env.DB_HOST,
-    DB_USER: process.env.DB_USER,
-    DB_PASSWORD: process.env.DB_PASSWORD,
-    DB_NAME: process.env.DB_NAME,
-    DB_PORT: process.env.DB_PORT || 5432,
-    DB_SSL: process.env.DB_SSL === 'true' ? { 
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 5432,
+    ssl: process.env.DB_SSL === 'true' ? { 
       rejectUnauthorized: false 
     } : false
   }
